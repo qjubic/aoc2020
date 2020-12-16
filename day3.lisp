391ec99d6c44c3a40fc84b5ba66fae63956c2a06
Louis Zakrzewski <l.zakrzewski@hotmail.de>
2020-12-16 11:10:58 +0100

    finished part1, inlined functions

--- day3.lisp
+++ day3.lisp
@@ -1,6 +1,6 @@
 ;;;https://adventofcode.com/2020/day/3
 
-(defvar *input* "c:/Users/lzakr/projects/cl/input.txt")
+(defvar *input* "Pathtoinputfile")
 
 (defun get-input (input)
   (let ((x (with-open-file (in input)
@@ -9,5 +9,19 @@
                    collect line))))
     x))
 
-(defun slide (hstep vstep input)
-  (loop ))
+(defun slide-down (input hstep vstep turn trees)
+  (labels ((slide (h-pos v-pos)
+             (if (char-equal #\# (char (elt input v-pos) h-pos)) 1 0))
+           (find-h-pos ()
+             (let ((x (* turn hstep))
+                   (y (length (first input))))
+               (if (< x y) x (mod x y))))
+           (find-v-pos ()
+             (* turn vstep)))
+    (let ((v-pos (find-v-pos)))
+      (if (>= v-pos (length input)) (values trees turn)
+          (slide-down input hstep vstep (1+ turn) (+ trees (slide (find-h-pos) v-pos)))))))
+
+
+(defun day3p1 ()
+  (slide-down (get-input *input*) 3 1 1 0))
