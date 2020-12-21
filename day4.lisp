b3f5d18bb75b663ed5e6b96f738b8f2b3e58b8dc
Louis Zakrzewski <qjube@cube.Speedport_W_724V_Typ_A_05011603_06_003>
2020-12-21 22:49:17 +0100

    progressed into exercise, put in verify-part and verify-passport

--- day4.lisp
+++ day4.lisp
@@ -1,11 +1,29 @@
 https://adventofcode.com/2020/day/4
 
-(defvar *input* "c:/Users/lzakr/projects/cl/input.txt")
+(defvar *input* "~/Projects/input.txt")
+(defvar *pdata* '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" )) ;; no cid, since optional 
 
 (defun get-input (input)
   (let ((x (with-open-file (in input)
              (loop for line = (read-line in nil)
                    while line
-                   collect (substitute #\space #\: line))))) ;;evtl not a good idea
+                   collect (substitute #\space #\: line))))) ;;columns are annoying, not necessary
     x))
+
+(defun verify-part (sublist testlist)
+  "Checks a string against a list of substrings and returns a number of matches."
+  (labels ((tester ()
+             (lambda (x) (if (search x sublist) 1 0))))
+    (reduce #'+ (map 'list (tester) testlist))))
+
+(defun verify-passport (data pos criteria sum)
+  (let ((part (elt data pos)))
+    (if (= (length part) 0) (list sum pos)
+        (verify-passport data (1+ pos) criteria (+ sum (verify-part part criteria))))))
+
+(defun day4p1 (passportdata criteria)
+  (let ((x (length passportdata))
+        (y (length criteria))
+        ())
+    ()))
 
