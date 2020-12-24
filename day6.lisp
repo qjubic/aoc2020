2cb5c5ab81c893bf9cf59bba26ce14a4850f2739
qjubic <louiszakrzewski@gmail.com>
2020-12-24 19:02:04 +0100

    solved part 1

--- day6.lisp
+++ day6.lisp
@@ -8,3 +8,14 @@
                    while line
                    collect line))))
     x))
+
+(defun get-unique-group-answers (inputlist &Optional (pos 0) (resultstring ""))
+  "Merges all answers by one group an removes duplicates, returns sum of answers and last position in inputlist"
+  (if (or (>= pos (length inputlist))
+          (= 0 (length (elt inputlist pos)))) (values (length resultstring) (1+ pos))
+      (get-unique-group-answers inputlist (1+ pos) (remove-duplicates (sort (format 'nil "~A~A" (elt inputlist pos) resultstring) #'char-lessp)))))
+
+(defun sum-of-all-groups-yes (inputlist &Optional (start 0) (sum 0))
+  (if (>= start (length inputlist)) sum
+      (multiple-value-bind (a b) (get-unique-group-answers inputlist start)
+        (sum-of-all-groups-yes inputlist b (+ sum a)))))
